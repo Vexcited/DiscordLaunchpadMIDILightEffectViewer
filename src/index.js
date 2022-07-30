e3a94a2e16eb27f868c4aa9fbe186420a0b5a25b
Mikkel RINGAUD <mikkel@milescode.dev>
2022-07-30 18:06:33 +0200

    refactor: cleanup

--- src/index.js
+++ src/index.js
@@ -7,17 +7,3 @@ import DiscordLaunchpadMIDILightEffectViewer from "./plugin/main";
 
 export default (() => isMissingLibrary() ? MissingLibraryLoader : DiscordLaunchpadMIDILightEffectViewer)();
 
-// /** @type {MIDIAccess | null} */
-// const getOutputs = () => {
-//   if (!_WEBMIDI_ACCESS_) return [];
-//   /** @type {MIDIOutput[]} */
-//   const outputs = [];
-
-//   for (const entry of _WEBMIDI_ACCESS_.outputs) {
-//     const output = entry[1];
-//     outputs.push(output);
-//   }
-
-//   return outputs;
-// }
-
