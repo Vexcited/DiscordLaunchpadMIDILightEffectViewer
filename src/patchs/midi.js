fd6d88bbac111f24264afac1e3a1f42700e999e1
Mikkel RINGAUD <mikkel@milescode.dev>
2022-07-30 21:44:33 +0200

    fix: remove backup file to prevent conflicts

--- src/patchs/midi.js
+++ src/patchs/midi.js
@@ -5,7 +5,8 @@ const { resourcesPath } = window.require("process");
 const { ipcRenderer } = window.require("electron");
 const fs = window.require("fs");
 
-const getAppFileCode = () =>  fs.readFileSync(`${resourcesPath}/app/index.js`, { encoding: "utf8" });
+/** @type {() => string} */
+const getAppFileCode = () => fs.readFileSync(`${resourcesPath}/app/index.js`, { encoding: "utf8" });
 
 export const checkMidiPermissionsInjector = () => {
   /** @type {string} */
@@ -26,12 +27,10 @@ export const injectMidiPermissions = () => {
   console.log("[WebMidiInjector] Injecting MIDI permissions...");
   
   const appFileCode = getAppFileCode();
-  const appFileCodePatched = appFileCode + "\n" + PATCH_CODE;
+  const appFileCodePatched = appFileCode + PATCH_CODE;
   
   // Write the patched file back to the app folder.
   fs.writeFileSync(`${resourcesPath}/app/index.js`, appFileCodePatched);
-  // Gonna keep a backup.
-  fs.writeFileSync(`${resourcesPath}/app/index.js.bak`, appFileCode);
 
   console.log("[WebMidiInjector] Injected new MIDI permissions ! Please, restart BetterDiscord.");
   BdApi.showConfirmationModal(
@@ -49,7 +48,13 @@ export const injectMidiPermissions = () => {
 
 export const removeMidiPermissions = () => {
   console.log("[WebMidiInjector] Removing MIDI permissions...");
-  fs.renameSync(`${resourcesPath}/app/index.js.bak`, `${resourcesPath}/app/index.js`);
+  
+  const appFileCode = getAppFileCode();
+  const appFileCodeCleaned = appFileCode.substring(
+    0, appFileCode.indexOf("\n// _WEBMIDI_PATCH_START_")
+  );
+
+  fs.writeFileSync(`${resourcesPath}/app/index.js`, appFileCodeCleaned);
   console.log("[WebMidiInjector] Removed MIDI permissions.");
 }
 
