5d9930569558cd8722975e2f543732d41c5c4bca
Mikkel RINGAUD <mikkel@milescode.dev>
2022-08-04 22:41:32 +0200

    feat: add color conversion for the UI and fix flickering

--- src/patchs/DlpeAttachment.js
+++ src/patchs/DlpeAttachment.js
@@ -6,7 +6,7 @@ import Launchpad from "../launchpads";
 
 import midiFileParser from "../utils/midiFileParser";
 import { devicesConfiguration } from "../utils/devices";
-import { DEFAULT_RGB_UI_PAD, novationLaunchpadPalette } from "../utils/palettes";
+import { DEFAULT_RGB_UI_PAD } from "../utils/palettes";
 
 class DlpeAttachment extends BdApi.React.Component {
   constructor(props) {
@@ -100,17 +100,40 @@ class DlpeAttachment extends BdApi.React.Component {
         let toWait = groupStartTime - (performance.now() - playTimeStart);
         await new Promise(r => setTimeout(r, toWait));
 
-        group.notes.forEach(note => {
+        for (const note of group.notes) {
           // Get the pad element from the Launchpad.
           const pad = launchpad.querySelector(`[data-note="${note.index}"]`);
-          if (!pad) return;
+          if (!pad) continue;
           
-          const color = note.color;
-          const colored_pad_style = `rgb(${color.join(", ")})`;
+          const colored_pad_style = `rgb(${note.uiColor.join(", ")})`;
 
           // Set the color of the pad for the `noteon`.
           pad.style.backgroundColor = colored_pad_style;
-        });
+
+          // Setup the timing for the `noteoff`.
+          setTimeout(() => {
+            const current_style = pad.style.backgroundColor;
+            if (!current_style) return;
+
+            /**
+             * Check if the pad haven't been triggered.
+             * If triggered with another color, then we do nothing
+             * and let the other trigger, handle everything.
+             * If it's still the same color, we remove it.
+             */
+            if (current_style === colored_pad_style) {
+              // Remove the color of the pad for the `noteoff`.
+              pad.style.backgroundColor = `rgb(${DEFAULT_RGB_UI_PAD.join(", ")})`;
+
+              if (device.output) {
+                const sysex = device_configuration.rgb_sysex([{
+                  note: note.index, color: [0, 0, 0]
+                }]);
+                device.output.sendSysex([], sysex);
+              }
+            }
+          }, note.duration);
+        }
       }
     }
 
--- src/utils/midiFileParser.js
+++ src/utils/midiFileParser.js
@@ -1,5 +1,10 @@
 import { convertNoteLayout } from "./layouts";
 import { novationLaunchpadPalette } from "./palettes";
+import { getUIColor } from "./colors";
+
+export const checkIsNoteOff = (note) => {
+  return note.color[0] === 0 && note.color[1] === 0 && note.color[2] === 0;
+};
 
 /** @param {ArrayBuffer} file */
 const midiFileParser = async (file) => {
@@ -25,55 +30,116 @@ const midiFileParser = async (file) => {
   // Group the notes by time.
   notes_data.forEach(note => {
     const start_time = note.time * 1000;
-    const duration = (note.duration * 1000) //+ delay;
+    const duration = (note.duration * 1000) + delay;
 
     const convert_results = convertNoteLayout(note.midi, "drum_rack", "programmer");
     if (!convert_results.success) return;
 
+    const colorFromPalette = novationLaunchpadPalette[note.velocity * 127];
+
     const parsed_noteon = {
       index: convert_results.note,
-      color: novationLaunchpadPalette[note.velocity * 127],
+      duration,
+      color: colorFromPalette,
+      uiColor: getUIColor(colorFromPalette)
     };
+    
+    // const parsed_noteoff = {
+    //   index: convert_results.note,
+    //   duration: 0,
+    //   color: [0, 0, 0]
+    // };
 
     const group_on = grouped_notes.find(
       group => group.start_time === start_time
     );
+
+    // const group_off = grouped_notes.find(
+    //   group => group.start_time === start_time + duration
+    // );
 
     if (!group_on) {
       grouped_notes.push({
         start_time,
         notes: [parsed_noteon]
       });
+    } else group_on.notes.push(parsed_noteon);
 
-      return;
-    }
-
-    group_on.notes.push(parsed_noteon);
-
-    const parsed_noteoff = {
-      index: convert_results.note,
-      color: [0, 0, 0]
-    };
-
-    const group_off = grouped_notes.find(
-      group => group.start_time === start_time + duration
-    );
-
-    if (!group_off) {
-      grouped_notes.push({
-        start_time: start_time + duration,
-        notes: [parsed_noteoff]
-      });
-
-      return;
-    }
-
-    group_off.notes.push(parsed_noteoff);
+    // if (!group_off) {
+    //   grouped_notes.push({
+    //     start_time: start_time + duration,
+    //     notes: [parsed_noteoff]
+    //   });
+    // } else group_off.notes.push(parsed_noteoff);
   });
 
-  console.log(grouped_notes);
-
   return grouped_notes;
+
+  // Cleanup the grouped notes.
+  const cleanedGroupedNotes = [];
+
+  // Check if a note is noteoff in the next group that it is currently on.
+  for (const group of grouped_notes) {
+    for (const note of group.notes) {
+      if (checkIsNoteOff(note)) continue;
+
+      const startTime = group.start_time;
+      const startEnd = startTime + note.duration;
+
+      const groupsWithinTheStartAndEnd = grouped_notes.filter(
+        _group => _group.start_time > startTime && _group.start_time <= startEnd + delay 
+      );
+
+      let addNoteOff = true;
+      
+      for (const groupProfiled of groupsWithinTheStartAndEnd) {
+        for (const noteProfiled of groupProfiled.notes) {
+          // When we find a note on, remove the noteoff of the current note.
+          if (noteProfiled.index === note.index && !checkIsNoteOff(noteProfiled)) {
+            addNoteOff = false;
+            console.log("lets not add", noteProfiled.index);
+          }
+        }
+      }
+
+      const group_on = cleanedGroupedNotes.find(
+        group => group.start_time === startTime
+      );
+  
+      const parsed_noteon = {
+        index: note.index,
+        color: note.color
+      };
+  
+      if (!group_on) {
+        cleanedGroupedNotes.push({
+          start_time: startTime,
+          notes: [parsed_noteon]
+        });
+      } else group_on.notes.push(parsed_noteon);
+  
+      if (addNoteOff) {
+        const group_off = cleanedGroupedNotes.find(
+          group => group.start_time === startEnd
+        );
+
+        const parsed_noteoff = {
+          index: note.index,
+          color: [0, 0, 0]
+        };
+
+        if (!group_off) {
+          cleanedGroupedNotes.push({
+            start_time: startEnd,
+            notes: [parsed_noteoff]
+          });
+        } else group_off.notes.push(parsed_noteoff);
+      }
+    }
+  }
+
+  console.log(cleanedGroupedNotes, grouped_notes);
+  return cleanedGroupedNotes;
 }
 
 export default midiFileParser;
